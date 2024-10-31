da69254c0b8005ce2e826631ca3a7d0363fd718e
Blate00 <da.coo@duocuc.cl>
2024-10-31 19:19:07 -0300

    trae proyectos relacionados con clientes, registra empleados, todo de manera local,

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const cors = require('cors'); // Importa el paquete cors
-const app = express();
-const clientRoutes = require('./routes/clientRoutes');
+   const express = require('express');
+   const cors = require('cors');
+   const app = express();
 
-app.use(cors()); // Habilita CORS para todas las rutas
-app.use(express.json()); // Para poder recibir JSON en las solicitudes
-app.use('/api', clientRoutes); // Asegúrate de que las rutas estén montadas correctamente
+   // Importa las rutas
+   const clientRoutes = require('./routes/clientRoutes');
+   const empleadoRoutes = require('./routes/empleadoRoutes');
+   const bancoRoutes = require('./routes/bancoRoutes'); // Nueva ruta
+   const tipoCuentaRoutes = require('./routes/tipoCuentaRoutes'); // Nueva ruta
+   const projectRoutes = require('./routes/projectRoutes')
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+   app.use(cors());
+   app.use(express.json());
+
+   // Monta las rutas
+   app.use('/api', clientRoutes);
+   app.use('/api', empleadoRoutes);
+   app.use('/api', bancoRoutes); // Nueva ruta
+   app.use('/api', tipoCuentaRoutes); // Nueva ruta
+   app.use('/api', projectRoutes);
+
+   const PORT = process.env.PORT || 5000;
+   app.listen(PORT, () => {
+       console.log(`Server running on port ${PORT}`);
+   });
