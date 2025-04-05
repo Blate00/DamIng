8ec6fbb9078e8a41e6c4fb105060eb6a787a3f3b
Blate0 <da.coo@duoc.cl>
2025-04-04 22:37:58 -0300

    fixx

--- src/config/api.js
+++ src/config/api.js
@@ -65,3 +65,10 @@ export const apiConfig = {
 export const api = axios.create({
   baseURL: API_URL,
 });
+api.interceptors.response.use(
+   response => response,
+   error => {
+       console.error('API Error:', error);
+       return Promise.reject(error);
+   }
+);
